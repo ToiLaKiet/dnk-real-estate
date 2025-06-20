e309e2e857b77e07b30033ddb81ffea2ab167b54
ToiLaKiet <tuilakietday05@gmail.com>
2025-06-20 11:06:20 +0700

    f

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
 
-export const API_URL = process.env.NEXT_PUBLIC_API_URL;
+export const API_URL = process.env.REACT_APP_API_URL;
 export const cloudName = 'df8ry7bhf';
 export const uploadPreset = 'dnk_real_estate';
